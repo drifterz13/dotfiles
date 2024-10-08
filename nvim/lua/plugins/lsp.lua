df36e233daba7904ecfcfed434deb28a62a91f28
drifterz13 <mixmyxxe@gmail.com>
2024-10-08 13:22:20 +0700

    update plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -101,7 +101,7 @@ return {
       })
 
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "eslint" },
+        ensure_installed = { "lua_ls" },
         handlers = {
           function(server_name)
             require("lspconfig")[server_name].setup({})
