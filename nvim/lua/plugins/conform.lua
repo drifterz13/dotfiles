db883d035145aaf64b6f420aa830e4a218185f40
drifterz13 <mixmyxxe@gmail.com>
2025-07-27 21:28:59 +0700

    chore: support css, scss

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,6 +7,8 @@ return {
 				javascript = { "prettierd", stop_after_first = true },
 				typescript = { "prettierd", stop_after_first = true },
 				typescriptreact = { "prettierd", stop_after_first = true },
+				css = { "prettierd", stop_after_first = true },
+				scss = { "prettierd", stop_after_first = true },
 				eruby = { "erb_format" },
 				ruby = { "rubocop" },
 			},
